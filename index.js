e35cfd0127288e749aa7e0e2feb9e0d3442eea06
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2020-10-02 19:16:15 -0400

    formatting

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-console.log(process.env.PORT);
 http.listen(process.env.PORT || 3000);
 
 io.on('connection', (socket) => {
-    console.log("User Connected");
-    io.emit('message', {name: 'ADMIN', 'text': 'a user has joined the chat!'})
+    io.emit('message', {'name': 'ADMIN', 'text': 'a user has joined the chat!'})
     
     socket.on('message', (data)=>{
-        console.log(data);
         io.emit('message', data)
     })  
 
     socket.on('disconnect', ()=>{
-        console.log("User Disconnected");
         io.emit('message', {name: 'ADMIN', 'text': 'a user has left the chat.'})
     })
 });
