0e8459996578c20ff6df893e2690103f4c463f85
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2020-10-02 18:55:12 -0400

    reee

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-http.listen(8080);
+http.listen(process.env.PORT);
 
 io.on('connection', (socket) => {
     console.log("User Connected");
