9aa9ddc016d289faf9d8553e9ca5c0d6de52a4d2
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2020-10-02 18:47:15 -0400

    eee

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+http.listen(8080);
+
+io.on('connection', (socket) => {
+    console.log("User Connected");
+    io.emit('message', {name: 'ADMIN', 'text': 'a user has joined the chat!'})
+    
+    socket.on('message', (data)=>{
+        console.log(data);
+        io.emit('message', data)
+    })  
+
+    socket.on('disconnect', ()=>{
+        console.log("User Disconnected");
+        io.emit('message', {name: 'ADMIN', 'text': 'a user has left the chat.'})
+    })
+});
